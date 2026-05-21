6536913789b2b4ff9413eb2999e9a53d97ce03ee
joel-20-joe <joelmusinguzi20@gmail.com>
2026-05-21 13:46:51 +0400

    added apps

--- userAccounts/models.py
+++ userAccounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# # Create your models here.
+# class user(customuser): 
+#     phone_number = models.CharField(max_length=15, blank=True)
+
+# class Tenant(models.model):
+    
+# class Agent(models.model):
+#     phone_number = models.CharField(max_length=15, blank=True)
+# class PropertyOwner(models.model):
+# class Buyer(models.model): 
